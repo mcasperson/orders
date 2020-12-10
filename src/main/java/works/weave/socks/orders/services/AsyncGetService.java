e9df71bf366b5fb56d09c1a92413f1f29d5d71eb
Matthew Casperson <matthewcasperson@gmail.com>
2020-12-11 05:41:14 +1000

    working on migrating to lambda

--- src/main/java/works/weave/socks/orders/services/AsyncGetService.java
+++ src/main/java/works/weave/socks/orders/services/AsyncGetService.java
@@ -10,6 +10,7 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.hal.Jackson2HalModule;
+import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
 import org.springframework.hateoas.mvc.TypeReferences;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
